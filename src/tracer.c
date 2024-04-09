e013b9e979e189b5da8fbcc7e0aceb35665bdc47
Lucas Lazaroiu <lucas.lazaroiu@stud.acs.pub.ro>
2024-04-09 23:42:01 +0300

    kmalloc handlers and finished data structs

--- src/tracer.c
+++ src/tracer.c
@@ -17,242 +17,6 @@ MODULE_DESCRIPTION("Kprobe based tracer");
 MODULE_AUTHOR("LAZAROIU LUCAS <lucas.lazaroiu@stud.acs.upb.ro");
 MODULE_LICENSE("GPL v2");
 
-// static int kmalloc_probe_entry_handler(struct kretprobe_instance *p, struct pt_regs *regs) {
-// 	return 0;
-// }
-
-// static int kmalloc_probe_handler(struct kretprobe_instance *p, struct pt_regs *regs) {
-//     struct process_data *pdata;
-//     size_t size = regs_return_value(regs);
-//     pid_t pid = current->pid;
-
-//     //mutex_lock(&process_table_lock);
-//     hash_for_each_possible(process_table, pdata, hlist, pid) {
-//         if (pdata->pid == pid) {
-//             pdata->kmalloc_count++;
-//             pdata->kmalloc_mem_total += size;
-//             break;
-//         }
-//     }
-//     //mutex_unlock(&process_table_lock);
-//     return 0;
-// }
-
-// static int kfree_probe_entry_handler(struct kretprobe_instance *p, struct pt_regs *regs) {
-//     return 0;
-// }
-
-// static int kfree_probe_handler(struct kretprobe_instance *p, struct pt_regs *regs) {
-//     struct process_data *pdata;
-//     void *addr = (void*)regs->di;
-//     size_t size = regs->di;
-
-//     //mutex_lock(&process_table_lock);
-//     hash_for_each_possible(process_table, pdata, hlist, current->pid) {
-//         if (pdata->pid == current->pid) {
-//             pdata->kfree_count++;
-//             pdata->kfree_mem_total += size;
-//             break;
-//         }
-//     }
-//     //mutex_unlock(&process_table_lock);
-//     return 0;
-// }
-
-// static int schedule_probe_handler(struct kretprobe_instance *p, struct pt_regs *regs) {
-//     struct process_data *pdata;
-//     pid_t pid = current->pid;
-
-//     //mutex_lock(&process_table_lock);
-//     hash_for_each_possible(process_table, pdata, hlist, pid) {
-//         if (pdata->pid == pid) {
-//             pdata->schedule_count++;
-//             break;
-//         }
-//     }
-//     //mutex_unlock(&process_table_lock);
-//     return 0;
-// }
-
-// static int up_probe_handler(struct kretprobe_instance *p, struct pt_regs *regs) {
-//     struct process_data *pdata;
-//     pid_t pid = current->pid;
-
-//     //mutex_lock(&process_table_lock);
-//     hash_for_each_possible(process_table, pdata, hlist, pid) {
-//         if (pdata->pid == pid) {
-//             pdata->up_count++;
-//             break;
-//         }
-//     }
-//     //mutex_unlock(&process_table_lock);
-//     return 0;
-// }
-
-// static int down_probe_handler(struct kretprobe_instance *p, struct pt_regs *regs) {
-//     struct process_data *pdata;
-//     pid_t pid = current->pid;
-
-//     //mutex_lock(&process_table_lock);
-//     hash_for_each_possible(process_table, pdata, hlist, pid) {
-//         if (pdata->pid == pid) {
-//             pdata->down_count++;
-//             break;
-//         }
-//     }
-//     //mutex_unlock(&process_table_lock);
-//     return 0;
-// }
-
-// static int mutex_lock_probe_handler(struct kretprobe_instance *p, struct pt_regs *regs) {
-//     struct process_data *pdata;
-//     pid_t pid = current->pid;
-
-//     //mutex_lock(&process_table_lock);
-//     hash_for_each_possible(process_table, pdata, hlist, pid) {
-//         if (pdata->pid == pid) {
-//             pdata->lock_count++;
-//             break;
-//         }
-//     }
-//     //mutex_unlock(&process_table_lock);
-//     return 0;
-// }
-
-// static int mutex_unlock_probe_handler(struct kretprobe_instance *p, struct pt_regs *regs) {
-//     struct process_data *pdata;
-//     pid_t pid = current->pid;
-
-//     //mutex_lock(&process_table_lock);
-//     hash_for_each_possible(process_table, pdata, hlist, pid) {
-//         if (pdata->pid == pid) {
-//             pdata->unlock_count++;
-//             break;
-//         }
-//     }
-//     //mutex_unlock(&process_table_lock);
-//     return 0;
-// }
-
-// // Define kretprobes
-
-// static struct kretprobe kmalloc_probe = {
-//     .kp = {
-//         .symbol_name = "__kmalloc",
-//     },
-//     .entry_handler = kmalloc_probe_entry_handler,
-//     .handler = kmalloc_probe_handler,
-// 	.maxactive = MAX_PROBES,
-// };
-
-// static struct kretprobe kfree_probe = {
-//     .kp = {
-//         .symbol_name = "kfree",
-//     },
-//     .entry_handler = kfree_probe_entry_handler,
-//     .handler = kfree_probe_handler,
-// 	.maxactive = MAX_PROBES,
-// };
-
-// static struct kretprobe schedule_probe = {
-//     .kp = {
-//         .symbol_name = "schedule",
-//     },
-//     .handler = schedule_probe_handler,
-// 	.maxactive = MAX_PROBES,
-// };
-
-// static struct kretprobe up_probe = {
-//     .kp = {
-//         .symbol_name = "up",
-//     },
-//     .handler = up_probe_handler,
-// 	.maxactive = MAX_PROBES,
-// };
-
-// static struct kretprobe down_probe = {
-//     .kp = {
-//         .symbol_name = "down",
-//     },
-//     .handler = down_probe_handler,
-// 	.maxactive = MAX_PROBES,
-// };
-
-// static struct kretprobe mutex_lock_probe = {
-//     .kp = {
-//         .symbol_name = "mutex_lock_nested",
-//     },
-//     .handler = mutex_lock_probe_handler,
-// 	.maxactive = MAX_PROBES,
-// };
-
-// static struct kretprobe mutex_unlock_probe = {
-//     .kp = {
-//         .symbol_name = "mutex_unlock_nested",
-//     },
-//     .handler = mutex_unlock_probe_handler,
-// 	.maxactive = MAX_PROBES,
-// };
-
-// IOCTL functions
-// static long tracer_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
-//     struct process_data *pdata;
-//     pid_t pid;
-
-//     switch (cmd) {
-//         case TRACER_ADD_PROCESS:
-//             if (copy_from_user(&pid, (pid_t *)arg, sizeof(pid_t)))
-//                 return -EFAULT;
-//             pdata = kmalloc(sizeof(struct process_data), GFP_KERNEL);
-//             if (!pdata)
-//                 return -ENOMEM;
-//             memset(pdata, 0, sizeof(struct process_data));
-//             pdata->pid = pid;
-//             //mutex_lock(&process_table_lock);
-//             hash_add(process_table, &pdata->hlist, pid);
-//             //mutex_unlock(&process_table_lock);
-//             // list_add_tail(&pdata->hlist, &tracked_processes);
-//             break;
-
-//         case TRACER_REMOVE_PROCESS:
-//             if (copy_from_user(&pid, (pid_t *)arg, sizeof(pid_t)))
-//                 return -EFAULT;
-//             //mutex_lock(&process_table_lock);
-//             hash_for_each_possible(process_table, pdata, hlist, pid) {
-//                 if (pdata->pid == pid) {
-//                     hash_del(&pdata->hlist);
-// 					//list_del(&pdata->hlist);
-//                     kfree(pdata);
-//                     break;
-//                 }
-//             }
-//             //mutex_unlock(&process_table_lock);
-//             break;
-
-//         default:
-//             return -EINVAL;
-//     }
-//     return 0;
-// }
-
-
-// Procfs file functions
-// static int proc_tracer_show(struct seq_file *m, void *v) {
-//     struct process_data *pdata;
-
-//     //mutex_lock(&process_table_lock);
-//     hash_for_each_possible(process_table, pdata, hlist, 0) {
-//         seq_printf(m, "%d\t%d\t%d\t%zu\t%zu\t%d\t%d\t%d\t%d\t%d\n",
-//                    pdata->pid, pdata->kmalloc_count, pdata->kfree_count,
-//                    pdata->kmalloc_mem_total, pdata->kfree_mem_total,
-//                    pdata->schedule_count, pdata->up_count,
-//                    pdata->down_count, pdata->lock_count,
-//                    pdata->unlock_count);
-//     }
-//     //mutex_unlock(&process_table_lock);
-//     return 0;
-// }
-
 // static int tracer_init(void) {
 
 // 	int ret;
@@ -272,107 +36,33 @@ MODULE_LICENSE("GPL v2");
 //         goto cleanup_kfree_probe;
 //     }
 
-// 	ret = register_kretprobe(&schedule_probe);
-//     if (ret < 0) {
-//         pr_err("register_kretprobe schedule failed\n");
-//         goto cleanup_schedule_probe;
-//     }
-
-//     ret = register_kretprobe(&up_probe);
-//     if (ret < 0) {
-//         pr_err("register_kretprobe up failed\n");
-//         goto cleanup_up_probe;
-//     }
-
-//     ret = register_kretprobe(&down_probe);
-//     if (ret < 0) {
-//         pr_err("register_kretprobe down failed\n");
-//         goto cleanup_down_probe;
-//     }
-
-//     ret = register_kretprobe(&mutex_lock_probe);
-//     if (ret < 0) {
-//         pr_err("register_kretprobe mutex_lock failed\n");
-//         goto cleanup_mutex_lock_probe;
-//     }
-
-//     ret = register_kretprobe(&mutex_unlock_probe);
-//     if (ret < 0) {
-//         pr_err("register_kretprobe mutex_unlock failed\n");
-//         goto cleanup_mutex_unlock_probe;
-//     }
-
-// 	// Register device
-//     ret = misc_register(&tracer_dev);
-//     if (ret)
-// 	{
-//         pr_err("failed to register misc device\n");
-//         goto cleanup_misc_register;
-//     }
-
-// 	// Create /proc/tracer file
-// 	proc_tracer = proc_create("tracer", 0, NULL, &proc_tracer_fops);
-//     if (proc_tracer != 0) {
-//         pr_err("failed to create /proc/tracer\n");
-//         ret = -ENOMEM;
-//         goto cleanup_proc_create;
-//     }
-
-// 	goto no_errors;
-
-// cleanup_misc_register:
-//     misc_deregister(&tracer_dev);
-// cleanup_proc_create:
-// 	proc_remove(proc_tracer);
-// cleanup_mutex_unlock_probe:
-//     unregister_kretprobe(&mutex_unlock_probe);
-// cleanup_mutex_lock_probe:
-//     unregister_kretprobe(&mutex_lock_probe);
-// cleanup_down_probe:
-//     unregister_kretprobe(&down_probe);
-// cleanup_up_probe:
-//     unregister_kretprobe(&up_probe);
-// cleanup_schedule_probe:
-//     unregister_kretprobe(&schedule_probe);
 // cleanup_kfree_probe:
 //     unregister_kretprobe(&kfree_probe);
 // cleanup_kmalloc_probe:
 //     unregister_kretprobe(&kmalloc_probe);
 
-// no_errors:
-// 	return 0;
-// }
 
 // static void tracer_exit(void)
 // {
 // 	// Unregister kretprobes
 //     unregister_kretprobe(&kmalloc_probe);
 //     unregister_kretprobe(&kfree_probe);
-//     unregister_kretprobe(&schedule_probe);
-//     unregister_kretprobe(&up_probe);
-//     unregister_kretprobe(&down_probe);
-//     unregister_kretprobe(&mutex_lock_probe);
-//     unregister_kretprobe(&mutex_unlock_probe);
-
-//     // Remove device
-//     misc_deregister(&tracer_dev);
-
-//     // Remove /proc/tracer file
-//     remove_proc_entry("tracer", NULL);
 //     }
-
-// 	// TO DO CLEAN HASH
 // }
 
-#define MAX_PROBES 32
+#define MAX_PROBES 64
 #define PROC_HASH_SIZE 8
 #define MEM_HASH_SIZE 8
 #define TRACER_PROC_NAME "tracer"
+
+static char *hash_header = 
+	"PID   kmalloc kfree kmalloc_mem kfree_mem  sched   up     down  lock   unlock\n";
 
 DEFINE_RWLOCK(lock); /**< Spinlock for protecting hashtable operations */
 
 // The /proc entry used for tracking statistics
 static struct proc_dir_entry *proc_tracer;
+static pid_t current_module_pid;
 
 /* hashtable structure for keeping address-size pairs
 
@@ -389,7 +79,11 @@ static struct mem_data *mem_data_alloc(unsigned int address, unsigned int size)
 {
 	struct mem_data *md;
 	
-	md = kmalloc(sizeof(*md), GFP_KERNEL);
+	/* This will be called from a kprobe handler, which run with interrupts disabled,
+	so GFK_KERNEL may lead to sleeping, which may lead to DEADLOCK. Therefore, we use
+	the GFP_ATOMIC flag (sacrifice performance for safety).
+	*/
+	md = kmalloc(sizeof(*md), GFP_ATOMIC);
 	if (md == NULL)
 		return NULL;
 	md->address = address;
@@ -408,6 +102,13 @@ static struct mem_data *mem_data_alloc(unsigned int address, unsigned int size)
 struct pid_stats_data {
     unsigned int kmalloc_count;
     unsigned int kmalloc_total;
+	unsigned int kfree_count;
+	unsigned int kfree_total;
+	unsigned int sched_count;
+	unsigned int up_count;
+	unsigned int down_count;
+	unsigned int lock_count;
+	unsigned int unlock_count;
 	DECLARE_HASHTABLE(memory_dict, MEM_HASH_SIZE);
 };
 
@@ -458,12 +159,16 @@ static struct pid_data_hash *pid_data_hash_alloc(pid_t pid)
 		return NULL;
 	pdh->pid = pid;
 	pdh->stats = kmalloc(sizeof(*(pdh->stats)), GFP_KERNEL);
-	pdh->stats->kmalloc_count = pid - 50;
-	pdh->stats->kmalloc_total = pid - 20;
+	pdh->stats->kmalloc_count = 0;
+	pdh->stats->kmalloc_total = 0;
+	pdh->stats->kfree_total = 0;
+	pdh->stats->kfree_count = 0;
+	pdh->stats->sched_count = 0;
+	pdh->stats->up_count = 0;
+	pdh->stats->down_count = 0;
+	pdh->stats->lock_count = 0;
+	pdh->stats->unlock_count = 0;
 	hash_init(pdh->stats->memory_dict);
-	mem_data_add(pdh, pdh->pid + 1000, pdh->pid + 150);
-	mem_data_add(pdh, pdh->pid + 1500, pdh->pid + 300);
-	mem_data_delete(pdh, pdh->pid + 1000);
 	return pdh;
 }
 
@@ -510,6 +215,22 @@ static void pid_data_hash_delete(pid_t pid)
 	write_unlock(&lock);
 }
 
+static struct pid_data_hash *pid_data_hash_search(pid_t pid)
+{
+	struct pid_data_hash *pdh;
+	struct hlist_node *q;
+
+	read_lock(&lock);
+	hash_for_each_possible_safe(pid_dict, pdh, q, hnode, pid) {
+		if (pdh->pid == pid) {
+			read_unlock(&lock);
+			return pdh;
+		}
+	}
+	read_unlock(&lock);
+	return NULL;
+}
+
 static void pid_data_purge_hash(void)
 {
 	struct pid_data_hash *pdh;
@@ -524,16 +245,80 @@ static void pid_data_purge_hash(void)
 	write_unlock(&lock);
 }
 
+/* structure that acts as a data interface between 
+the entry and regular handler */
+struct reg_data {
+	struct pid_data_hash *pdh;
+	unsigned int size;
+};
+
+// Handler functions for kretprobes
+
+static int kmalloc_probe_entry_handler(struct kretprobe_instance *p, struct pt_regs *regs)
+{	
+	struct reg_data *data = (struct reg_data *)p->data;
+	struct pid_data_hash *pdh;
+	
+	// blacklist the probe handler
+	if (current->pid == current_module_pid) {
+		data->size = -1;
+		return 0;
+	}
+
+	pdh = pid_data_hash_search(current->pid);
+	if (pdh == NULL) {
+		// don't instrument anything, we are not tracking this process
+		data->size = -1;
+		return 0;
+	}
+	data->size = regs->ax;
+	data->pdh = pdh;
+	return 0;
+}
+
+static int kmalloc_probe_handler(struct kretprobe_instance *p, struct pt_regs *regs)
+{	
+	unsigned int address;
+	struct reg_data *data = (struct reg_data *)p->data;
+
+	if (data->size == -1)
+		// entry_handler told us to not instrument this process
+		return 0;
+
+	// increase the call count and total memory
+	++data->pdh->stats->kmalloc_count;
+	data->pdh->stats->kmalloc_total += data->size;
+
+	// add an entry to the address-size dictionary 
+	address = regs_return_value(regs);
+	// we are in atomic context and should be careful when we allocate memory
+	mem_data_add(data->pdh, address, data->size);
+
+	return 0;
+}
+
+// Define kretprobes
+static struct kretprobe kmalloc_probe = {
+    .kp = {
+        .symbol_name = "__kmalloc",
+    },
+    .entry_handler = kmalloc_probe_entry_handler,
+    .handler = kmalloc_probe_handler,
+	.data_size = sizeof(struct reg_data),
+	.maxactive = MAX_PROBES,
+};
+
 // IOCTL function
 static long tracer_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
-{
+{	
+	current_module_pid = current->pid;
 	switch (cmd) {
 	case TRACER_ADD_PROCESS:
-		pr_info("ADD: cmd is %u, arg is %lu\n", cmd, arg);
+		pid_data_hash_add((pid_t)arg);
 		break;
 
 	case TRACER_REMOVE_PROCESS:
-		pr_info("REMOVE: cmd is %u, arg is %lu\n", cmd, arg);
+		pid_data_hash_delete((pid_t)arg);
 		break;
 
 	default:
@@ -554,24 +339,31 @@ static struct miscdevice tracer_dev = {
 	.fops = &tracer_fops,
 };
 
-// Procfs file functions
+// Procfs file function
 static int proc_tracer_show(struct seq_file *m, void *v)
 {
     struct pid_data_hash *pdh;
-	struct mem_data *md;
-	struct hlist_node *q, *q2;
-	unsigned int bucket, bucket2;
-
-	seq_printf(m, "hash header\n");
-
+	struct hlist_node *q;
+	unsigned int bucket;
+	
+	seq_printf(m, "%s", hash_header);
+	read_lock(&lock);
     hash_for_each_safe(pid_dict, bucket, q, pdh, hnode) {
-        seq_printf(m, "pid is %u, stats are count: %d and total: %d\n",
-			pdh->pid, pdh->stats->kmalloc_count, pdh->stats->kmalloc_total);
-		seq_printf(m, "now going through memory hash:\n");
-		hash_for_each_safe(pdh->stats->memory_dict, bucket2, q2, md, hnode) {
-			seq_printf(m, "address is %u, size is: %u\n", md->address, md->size);
-		}
-    }
+		// we make sure the data sits nicely and is alligned in its column
+        seq_printf(m, "%-5u %-7u %-5u %-11u %-10u %-7u %-6u %-5u %-6u %-6u\n",
+			pdh->pid,
+			pdh->stats->kmalloc_count,
+			pdh->stats->kfree_count,
+			pdh->stats->kmalloc_total,
+			pdh->stats->kfree_total,
+			pdh->stats->sched_count,
+			pdh->stats->up_count,
+			pdh->stats->down_count,
+			pdh->stats->lock_count,
+			pdh->stats->unlock_count
+		);
+	}
+	read_unlock(&lock);
 	return 0;
 }
 
@@ -606,21 +398,25 @@ static int tracer_init(void)
 		pr_err("failed to create /proc/tracer\n");
 		goto cleanup_proc_create;
 	}
+	// register kretprobes
 
-    pid_data_hash_add(100);
-	pid_data_hash_add(101);
-	pid_data_hash_add(102);
-	pid_data_hash_delete(101);
+	ret = register_kretprobe(&kmalloc_probe);
+    if (ret) {
+        pr_err("register_kretprobe kmalloc failed\n");
+        goto cleanup_kmalloc_probe;
+    }
 
 	return 0;
 
-	// Cleanup after errors
-
+// Cleanup after errors
 cleanup_misc_register:
 	misc_deregister(&tracer_dev);
 	return ENOMEM;
 cleanup_proc_create:
 	proc_remove(proc_tracer);
+	return ENOMEM;
+cleanup_kmalloc_probe:
+	unregister_kretprobe(&kmalloc_probe);
 	return ENOMEM;
 }
 
@@ -634,6 +430,9 @@ static void tracer_exit(void)
 
 	// Free up memory allocated in pid_dict
 	pid_data_purge_hash();
+
+	// Unregister probes
+	unregister_kretprobe(&kmalloc_probe);
 }
 
 module_init(tracer_init);
